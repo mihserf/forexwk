54a2e9713cb81d14b93f9bfd2b4f1d6307b942d0
Michail Gaganov <gaganov@gmail.com>
2008-12-25 18:18:33 +0200

    pages fixes. editor added to article form

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,2 +1,46 @@
 class Admin::PagesController < ApplicationController
+  before_filter :admin_required
+
+
+
+  def index
+    @pages=Page.find(:all, :order => :permalink)
+    #render(:layout=>"admin")
+  end
+
+  def new
+    @page=Page.new
+  end
+
+  def create
+    @page=Page.new(params[:page])
+    if  @page.save!
+      flash[:notice]="Страница создана."
+      redirect_to admin_pages_path
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @page=Page.find(params[:id])
+    #render(:layout=>"admin")
+  end
+
+  def update
+    @page=Page.find(params[:id])
+    if  @page.update_attributes(params[:page])
+      flash[:notice]="страница обновлена"
+      redirect_to admin_pages_path
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    respond_to do |format|
+      format.html { redirect_to(admin_pages_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,22 @@
 class PagesController < ApplicationController
+  def show
+    page = params[:id]
+    @page = Page.find_by_permalink(params[:id])
+	if File.exists?("#{RAILS_ROOT}/app/views/pages/#{page}.html.erb")
+
+                render :action => page
+
+                #redirect_to :action => page
+        end
+
+  end
+
+
+  def home
+    @page=Page.find_by_permalink("home")
+  end
+
+  def contacts
+    @page=Page.find_by_permalink("contacts")
+  end
 end
