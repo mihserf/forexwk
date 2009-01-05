989fb59ac88a84c2104289b44cb6eafd3c8ce8ce
Michail Gaganov <gaganov@gmail.com>
2009-01-05 13:16:31 +0200

    gitignore

--- app/models/action.rb
+++
@@ -1,4 +0,0 @@
-class Action < ActiveRecord::Base
-  has_many :user_actions, :dependent => :destroy
-  has_many :users, :through => :user_actions
-end
--- app/models/user_action.rb
+++
@@ -1,4 +0,0 @@
-class UserAction < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :action
-end
