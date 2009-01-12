7f44eb5822d6a85ef862fc0217c2b7464b4d41ed
Michail Gaganov <gaganov@gmail.com>
2009-01-12 14:14:11 +0200

    ferret

--- app/models/dealing_center.rb
+++ app/models/dealing_center.rb
@@ -1,5 +1,5 @@
 class DealingCenter < ActiveRecord::Base
   has_many :users
   
-  acts_as_ferret :fields =>{:name=>{:boost=>4},:description=>{},:url=>{}},:store_class_name => true,:remote => true
+  acts_as_ferret :fields =>{:name=>{:boost=>4},:description=>{},:url=>{}},:store_class_name => true, :remote => true
 end
