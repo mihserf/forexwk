371721922b2babb470d8a066c4baeaf8cb8395c4
Michail Gaganov <gaganov@gmail.com>
2009-01-17 01:44:19 +0200

    forum

--- config/environment.rb
+++ config/environment.rb
@@ -74,5 +74,5 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 end
-require 'extensions.rb'
+require 'lib/extensions.rb'
 require 'lib/savage_beast/user_init.rb'
