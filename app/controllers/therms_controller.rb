3d0eec803f875fabac0de13bd9ecf65b76326485
Michail Gaganov <gaganov@gmail.com>
2009-01-21 22:28:49 +0200

    fixes

--- app/controllers/therms_controller.rb
+++ app/controllers/therms_controller.rb
@@ -1,5 +1,6 @@
 class ThermsController < ApplicationController
   def index
+
     per_page = 50
     
     alphabet=%w(А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Э Ю Я A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
