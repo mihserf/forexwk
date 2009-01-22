2f83ab0011f6e21a92209c94a26b312c7f149efc
Michail Gaganov <gaganov@gmail.com>
2009-01-22 14:02:41 +0200

    dealing center users counting

--- app/controllers/dealing_centers_controller.rb
+++ app/controllers/dealing_centers_controller.rb
@@ -1,5 +1,5 @@
 class DealingCentersController < ApplicationController
   def index
-    @dealing_centers = DealingCenter.paginate(:page => params[:page], :per_page => 20, :conditions=>["temp=?",false], :order=>"created_at DESC")
+    @dealing_centers = DealingCenter.paginate(:page => params[:page], :per_page => 20, :conditions=>["temp=?",false], :order=>"name")
   end
 end
