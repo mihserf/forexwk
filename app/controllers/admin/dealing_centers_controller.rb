7e0a868cd0aa1b8d8e083cfe102ef4269af2af29
michail <gaganov@gmail.com>
2009-02-26 08:53:25 +0200

    bagfix Диллинговый -> Дилинговый

--- app/controllers/admin/dealing_centers_controller.rb
+++ app/controllers/admin/dealing_centers_controller.rb
@@ -15,7 +15,7 @@ class Admin::DealingCentersController < ApplicationController
     @dealing_center=DealingCenter.new(params[:dealing_center])
 
     if  @dealing_center.save!
-      flash[:notice]="Диллинговый центр создан."
+      flash[:notice]="Дилинговый центр создан."
       redirect_to admin_dealing_centers_path
     else
       render :action => "new"
@@ -30,8 +30,8 @@ class Admin::DealingCentersController < ApplicationController
     @dealing_center=DealingCenter.find(params[:id])
 
     if  @dealing_center.update_attributes(params[:dealing_center])
-      if params[:rate]=="true" && current_user.moderator? && !@dealing_center.temp? then  User.find(:first, :conditions => {:id => @dealing_center.added_by_user}).add_rating(Settings.rating.bonus.for_dealing_center,User.find(:first,:conditions=>{:admin=>true}),"за диллинговый центр") end
-      flash[:notice]="Диллинговый центр обновлен"
+      if params[:rate]=="true" && current_user.moderator? && !@dealing_center.temp? then  User.find(:first, :conditions => {:id => @dealing_center.added_by_user}).add_rating(Settings.rating.bonus.for_dealing_center,User.find(:first,:conditions=>{:admin=>true}),"за дилинговый центр") end
+      flash[:notice]="Дилинговый центр обновлен"
       redirect_to admin_dealing_centers_path
     end
   end
