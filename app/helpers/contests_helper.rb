15124f191df723faa32adbd8db3b66610664353a
michail <gaganov@gmail.com>
2009-03-16 13:15:11 +0200

    rules one for all contests

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,6 +1,6 @@
 module ContestsHelper
   def contest_info(contest)
-    contest_rules = Page.find_by_permalink("contest_rules")
+    contest_rules = Page.find_by_permalink("contest_rules").content
     top_users = User.top(contest.id).all(:limit => 5)
     users_number = User.count(:joins =>:user_contests, :conditions =>["user_contests.contest_id=:contest_id",{:contest_id => @contest.id}])
     render :partial => "contests/contest_info", :locals => { :contest_rules => contest_rules, :top_users => top_users, :contest => contest, :users_number => users_number }
