fc87e6df242ac83a62031f2ae6411de7d2725f40
Michail Gaganov <gaganov@gmail.com>
2009-04-07 13:15:51 +0300

    closes #25, improvements

--- app/models/currency_data.rb
+++ app/models/currency_data.rb
@@ -1,5 +1,5 @@
 class CurrencyData < ActiveRecord::Base
   belongs_to :trend_data
   belongs_to :currency_pair
-  has_many :trends, :dependent => :destroy
+  has_many :trends, :order => "trend_type", :dependent => :destroy
 end
